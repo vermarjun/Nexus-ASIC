ecc8417757705334749784de336637c3ffae04ef
vermarjun <vermarjun26@gmail.com>
2024-10-28 22:56:29 +0530

    Main change to vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server : {
     proxy : {
-      '/api':'https://nexus-asic-backend.vercel.app',
+      '/api':'https://nexus-asic-backend.vercel.app/',
     },
   },
   plugins: [react()],
